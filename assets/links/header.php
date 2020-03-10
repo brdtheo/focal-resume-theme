9eb8d6aaf28eed10fbb648d0174ee22afa4ca0af
Théo BRD <toto.billardey@gmail.com>
2020-03-10 17:04:10 +0100

    subbar

--- assets/links/header.php
+++ assets/links/header.php
@@ -1,11 +1,14 @@
 <header class="main-header">
-    <div class="main-header-img"><!--image-->
 
-        <div class="img-div"></div>
-        <div class="main-header-img-infos">
-            <h1>web & graphic designer</h1>
-            <a href="#" class="button">learn more</a>
+        <div class="main-header-img"><!--image-->
+            <div class="main-header-img-infos">
+                <h1>web & graphic designer</h1>
+                <a href="#" class="button">learn more</a>
+            </div>
         </div>
-        
-    </div>
+
+        <div class="main-header-greybar">
+            
+        </div>
+        <nav></nav>
 </header>
