941c92dd4d277cb2acef2d821793c495cf4df259
Théo BRD <toto.billardey@gmail.com>
2020-03-10 09:25:52 +0100

    project files

---
+++ assets/links/head.php
@@ -0,0 +1,5 @@
+<head>
+    <title>Focal resume theme</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
---
+++ index.php
@@ -0,0 +1,1 @@
+<?php include_once 'assets/links/head.php' ?>
