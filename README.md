24605a781c810d5fa1cd7e5226dc47a5a751c566
Théo Billardey <theo.billardey@gmail.com>
2021-05-02 18:43:54 +0200

    rebase to gulp structure

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Focal resume theme
 
-Reproduction d'une template en SASS
-
-Demo: https://theo-billardey.fr/demos/focal-resume-theme/<br/>
-Maquette: https://imgur.com/a/NdUGKZK
+![](https://i.imgur.com/WpXOzuG.png)
--- assets/links/head.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Focal resume theme</title>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
